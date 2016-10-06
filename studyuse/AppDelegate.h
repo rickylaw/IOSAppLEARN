46689600ceef001b89b8d4a0ef406f3f65bff33a
Luo Jingli <JingLiLuo@jinglis-macbook-pro.local>
2016-10-06 14:43:38 -0400

    first

---
+++ studyuse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  studyuse
+//
+//  Created by Jingli Luo on 2016-09-08.
+//  Copyright © 2016 Jingli Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ studyuse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  studyuse
+//
+//  Created by Jingli Luo on 2016-09-08.
+//  Copyright © 2016 Jingli Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
